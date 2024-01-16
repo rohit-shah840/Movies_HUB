6e6105c35dc013b99a41287eb7dab546a73476f2
rohit-shah840 <rohitshah1455@gmail.com>
2024-01-16 12:54:35 +0530

    adding all the files and folder of movies hub

---
+++ src/App.js
@@ -0,0 +1,64 @@
+//d586dcae---api key
+import { useEffect, useState } from "react";
+
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=d586dcae";
+
+// const movie1 = {
+//   Title: "Fighting, Flying and Driving: The Stunts of Spiderman 3",
+//   Year: "2007",
+//   imdbID: "tt1132238",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg",
+// };
+
+const App = () => {
+  const [movies, setMoives] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMoives(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+  
+  return (
+    <div className="app">
+      <h1>Movies HUB</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="serach"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
